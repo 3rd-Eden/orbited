dde13375d593c29dbd88e186a9783e03b7f7d008
heyadayo <heyadayo@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-02-01 16:42:43 +0000

    ...

--- daemon/orbited/orbited-0.4.0-py2.5.egg/orbited/logger/__init__.py
+++
@@ -1,7 +0,0 @@
-from log import setup
-from orbited.config import map as config
-logroot = setup(config)
-
-
-def get_logger(name):
-    return logroot.get_logger(name)
--- daemon/orbited/orbited-0.4.0-py2.5.egg/orbited/transport.py
+++
@@ -1,3 +0,0 @@
-class TransportConnection(object):
-    pass
-    
