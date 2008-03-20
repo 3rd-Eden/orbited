718ce16b4576a0346b93441bb1fd5131c0d8cfd7
mario <mario@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-03-20 10:35:31 +0000

    preliminary stomp stuff...

--- daemon/orbited/stomp/daemon.py
+++ daemon/orbited/stomp/daemon.py
@@ -1,43 +1,53 @@
-from dez.op.server.server import OPServer
+from dez.stomp.server.server import STOMPServer
 from orbited.op.message import OrbitMessage
+from orbited.stomp.transaction import STOMPTransaction
 
-class OPDaemon(object):
-  
+class STOMPDaemon(object):
     def __init__(self, host, port, dispatcher):
-        self.server = OPServer(host, port)
+        self.server = STOMPServer(host, port)
         self.server.set_connect_cb(self.__connect_cb)
         self.dispatcher = dispatcher
         self.__send_cb = None
         self.connections = set()
-    
+        self.transactions = {}
+
     def __connect_cb(self, conn):
         conn.set_request_cb(self.__request_cb)
         conn.set_close_cb(self.__disconnect_cb, [conn])
         self.connections.add(conn)
-            
+
     def __disconnect_cb(self, conn):
         if conn in self.connections:
             self.connections.remove(conn)
-        
+
     def __request_cb(self, frame):
-        print 'FRAME:', frame.action
-        if frame.action == 'SEND':
-            recipients = frame.headers['recipients']
+        if frame.action == 'BEGIN':
+            self.transactions[frame.headers['transaction']] = STOMPTransaction(self,frame.headers['transaction'])
+        elif 'transaction' in frame.headers:
+            self.transactions[frame.headers['transaction']](frame)
+        elif frame.action == 'SEND':
+            recipients = frame.headers['destination']
             payload = frame.body
             m = OrbitMessage(recipients, payload, self.__message_cb, [frame])
             self.dispatcher.dispatch_orbit(m)
         frame.received()
-        
+
     def __message_cb(self, message, frame):
         if message.failure_recipients:
             frame.failure(message.failure_recipients)
         if message.success_recipients:
             frame.success(message.success_recipients)
 
+    def commit(self, frame):
+        self.__request_cb(frame)
+
+    def abort(self, trans_id):
+        del self.transactions[trans_id]
+
     def signon_cb(self, key):
         for conn in self.connections:
-            conn.signon_cb({'key': ",".join(key)})
-            
+            conn.signon_cb(",".join(key))
+
     def signoff_cb(self, key):
         for conn in self.connections:
-            conn.signoff_cb({'key': key})
+            conn.signoff_cb(",".join(key))
---
+++ daemon/orbited/stomp/transaction.py
@@ -0,0 +1,19 @@
+class STOMPTransaction(object):
+    def __init__(self, daemon, trans_id):
+        self.daemon = daemon
+        self.trans_id = trans_id
+        self.frames = []
+
+    def __call__(self, frame):
+        del frame.headers['transaction']
+        if 'receipt' in frame.headers:
+            frame.received()
+            del frame.headers['receipt']
+        if frame.action == "COMMIT":
+            for f in self.frames:
+                self.daemon.commit(f)
+            self.daemon.abort(self.trans_id)
+        elif frame.action == "ABORT":
+            self.daemon.abort(self.trans_id)
+        else:
+            self.frames.append(frame)
