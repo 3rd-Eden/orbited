9f46318b06171995ff2b132a5584d66acb3eb93d
heyadayo <heyadayo@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-03-22 13:20:28 +0000

    broken, but i need to switch computers...

--- daemon/orbited/cometwire.py
+++ daemon/orbited/cometwire.py
@@ -4,31 +4,61 @@ from orbited.json import json
 import random
 
 COMETWIRE_URL = config['[cometwire]']['url']
+CSP_UPSTREAM_URL = config['[csp]']['upstream_url']
 
+TIMEOUT = int(config['[cometwire]']['connect_timeout'])
+URLS = [ COMETWIRE_URL, CSP_UPSTREAM_URL ]
 
 class CometWire(object):
     """Maps a downstream connection to an upstream connection."""
     
     def __init__(self, dispatcher):
         self.dispatcher = dispatcher
-        self.dispatcher.app.transports.set_identifier_callback(COMETWIRE_URL, self.__client_connect_callback)
+        self.transports = self.dispatcher.app.transports
+        for url in URLS:
+            self.transports.set_identifier_callback(url, self.__client_connect_callback, [url])
+        self.callbacks = {}
+        self.downstream_connections = {}
+        self.timers = {}
+        
+    def set_connect_cb(self, url, cb, args):
+        self.callbacks[url] = cb, args
     
-    def __client_connect_callback(self):
+    def __client_connect_callback(self, url):
         key = self.__generate_key()
         initial_msgs = []
-        
         initial_msgs.append(SingleRecipientMessage(json.encode(key), (key, COMETWIRE_URL)))
         print "UPSTREAM -- SET CONNECT CB for COMETWIRE"
-        self.dispatcher.app.upstream.set_connect_cb(key, self.__upstream_connected, [key])
-        
+        self.dispatcher.app.upstream.set_connect_cb(key, self.__upstream_connected, [key, url])
+        self.timers[key] = event.timeout(TIMEOUT, self.__upstream_connect_timed_out, key)
         return (key, initial_msgs)
+        
+    def __upstream_connect_timed_out(self, key, url):
+        conn = self.transports.get(key)
+        # TODO: send a 'timeout' msg then close on success or failure of that msg?
+        conn.close()
+        self.timers[key].delete()
+        del self.timers[key]
+        print "COMETWIRE -- upstream connect timeout", key
         
     def __generate_key(self):
         return ''.join([random.choice("123456789ABCDEF") for i in range(10)])
         
     def __upstream_connected(self, conn, key):
-        print "UPSTREAM CONNECTED", key
-        conn.set_receive_callback(self.__upstream_receive_cb)
-    
+        # Are we waiting on this particular upstream connection?
+        if key not in self.timers:
+            # TODO: error...?
+            return
+        self.timers[key].delete()
+        del self.timers[key]
+        if url in self.callbacks:
+            upstream_conn = conn
+            downstream_conn = self.transports.get(key)
+            cb, args = self.callbacks[url]
+            return cb(key, upstream_conn, downstream_conn, *args)
+        else:
+            conn.set_receive_cb(self.__upstream_receive_cb)
+
     def __upstream_receive_cb(self, data):
         print "UPSTREAM ---- GOT DATA", data
+
