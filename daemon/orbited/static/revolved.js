78b4a01164908fcd8bc9d1995d502b9ba3bec837
frank <frank@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-04-04 01:16:16 +0000

    revolved.js and revolvedtest (preliminary)

---
+++ daemon/orbited/static/revolved.js
@@ -0,0 +1,53 @@
+Orbited.require("socket.js")
+
+Revolved = function() {
+    var self = this
+    
+    self.readyState = 0
+    
+    self.connect = function() {
+        self.conn = new TCPConnection(null, onopen, onread, onclose)
+    }
+    
+    
+    var onopen = function() {
+        self.readyState = 1
+    }
+    
+    var onread = function(s) {
+        shell.print("REVOLVED got: " + s)
+    }
+    
+    var onclose = function() {
+        
+    }
+    
+
+    self.auth = function() {
+        
+        send(["AUTH", []])
+    }
+
+    
+    self.publish = function(channel, s) {
+        send(["PUBLISH", [channel, s]])
+    }
+    
+    self.subscribe = function(channel) {
+        send(["SUBSCRIBE", [channel]])
+    }
+    
+    self.send = function(id, s) {
+        send(["SEND", [id, s]])
+    }
+    
+    self.disconnect = function() {
+        self.readyState = 2
+        conn.disconect()
+    }
+    
+    var send = function(s) {
+        conn.send(JSON.stringify(s))
+    }
+    
+}
