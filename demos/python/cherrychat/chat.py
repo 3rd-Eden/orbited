33e38c32ae72320a949f3b47ceab22b67f0c8992
heyadayo <heyadayo@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-04-07 16:54:40 +0000

    test for revolved callbacks

--- demos/python/cherrychat/chat.py
+++ demos/python/cherrychat/chat.py
@@ -1,27 +1,44 @@
 import cherrypy
 from client import OrbitClient
-orbit = OrbitClient()
-orbit.connect()
-orbit.callback("signon", "http://localhost:4700/orbited_signon")
-orbit.callback("signoff", "http://localhost:4700/orbited_signoff")
+from orbited.json import json
+#orbit = OrbitClient()
+#orbit.connect()
+#orbit.callback("signon", "http://localhost:4700/orbited_signon")
+#orbit.callback("signoff", "http://localhost:4700/orbited_signoff")
 
 class ChatServer(object):
-    users = []
-    def user_keys(self):
-        return [u for u in self.users]
+#    i = 0
+#    users = []
+#    def user_keys(self):
+#        return [u for u in self.users]
     
-    @cherrypy.expose
-    def orbited_signon(self, function, key):
-        if key not in self.users:
-            self.users.append(key)
-            orbit.send(self.user_keys(), '<b>%s joined</b>' % key)
+    
+#    @cherrypy.expose
+#    def orbited_signon(self, function, key):
+#        if key not in self.users:
+#            self.users.append(key)
+#            orbit.send(self.user_keys(), '<b>%s joined</b>' % key)
+            
+#    @cherrypy.expose
+#    def orbited_signoff(self, function, key):
+#        if key in self.users:
+#            self.users.remove(key)
+#            orbit.send(self.user_keys(), '<b>%s has left</b>' % key)
+            
             
     @cherrypy.expose
-    def orbited_signoff(self, function, key):
-        if key in self.users:
-            self.users.remove(key)
-            orbit.send(self.user_keys(), '<b>%s has left</b>' % key)
+    def revolved_auth_channel(self, user_key):
+        print 'user_key', user_key
+        return json.encode(dict(publish=True, subscribe=True))
             
+    @cherrypy.expose
+    def revolved_auth_connect(self, user_key, auth_data):
+      
+        print "user_key", json.decode(user_key)
+        print "auth_data", json.decode(auth_data)
+        return json.encode('max' in user_key)
+    
+        
 if __name__ == '__main__':
     import os
     # This code is straight from the cherrypy StaticContent wiki
