49587cf37c40d6bf6990510db5d61a75dfb72285
mario <mario@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-04-08 03:27:56 +0000

    index, 404

--- daemon/orbited/plugin.py
+++ daemon/orbited/plugin.py
@@ -26,7 +26,7 @@ class PluginManager(object):
                 # to view all classes defined in file, and use any
                 # class that inherits from Plugin
                 # (so you can define multiple classes in one file)
-                self.plugins[plugin_name] = plug.Main(self.dispatcher)
+                self.plugins[plugin_name] = plug.Main(plugin_name, self.dispatcher)
                 if 'active' in val and val['active']:
                     self.plugins[plugin_name]._activate()
 
@@ -34,14 +34,15 @@ class PluginMeta(type):
     def __init__(cls, name, bases, dct):
         if name != "Plugin":
             old__init__ = cls.__init__
-            def new__init__(self, app):
-                Plugin.__init__(self, app)
+            def new__init__(self, name, app):
+                Plugin.__init__(self, name, app)
                 old__init__(self)
             cls.__init__ = new__init__
 
 class Plugin(object):
     __metaclass__ = PluginMeta
-    def __init__(self, app):
+    def __init__(self, name, app):
+        self.__name = name
         self.__app = app
 
     def __dispatch(self, func):
@@ -49,7 +50,15 @@ class Plugin(object):
             getattr(self,func)(req)
         return cb
 
+    def __404(self, req):
+        req.write("404")
+        req.close()
+
     def _activate(self):
+        self.__app.add_cb_rule("/"+self.__name+"/",self.__404)
         for key, val in self.__class__.__dict__.items():
             if hasattr(val,'_exposed'):
-                self.__app.add_cb_rule("/"+key,self.__dispatch(key))
+                cb = self.__dispatch(key)
+                self.__app.add_cb_rule("/"+self.__name+"/"+key,cb)
+                if key == "index":
+                    self.__app.add_cb_rule("/"+self.__name,cb)
