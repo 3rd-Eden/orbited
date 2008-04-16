b59deb67d9c7dfaf932ec7f1b86b71e33d5d4b27
mario <mario@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-04-16 02:35:35 +0000

    plugin system complete

--- daemon/orbited/static/manage.js
+++ daemon/orbited/static/manage.js
@@ -18,25 +18,36 @@ function Manage() {
     }
 
     self.load = function(result) {
-        document.getElementById('plugins').innerHTML = make_links(result[1],"Manager","switch_plugin");
-        self.switch_plugin([result[0],result[1][0]]);        
+        document.getElementById('functions').innerHTML = make_links(['status','start','stop'],"Plugin.get('","',Manager.display_result);\">");
+        document.getElementById('plugins').innerHTML = make_links(result[1],"Manager.switch_plugin([[true,'plugin switched to: ","'],'","'])\">");
+        self.switch_plugin([result[0],result[1][0]]);
     }
 
     self.switch_plugin = function(result) {
-        Plugin = new Library("/"+result[1].replace(".","/")+"/manage/");
-        document.getElementById('name').innerHTML = result[1];
+        if (typeof(result[1]) != "undefined") {
+            Plugin = new Library("/_/"+result[1]+"/manage/");
+            document.getElementById('name').innerHTML = result[1];
+        }
+        else {
+            document.getElementById('name').innerHTML = "(none)";
+        }
         self.display_result(result[0]);
     }
 
-    var make_links = function(plugins) {
+    var make_links = function(items, prescript, midscript, postscript) {
+        if ( ! items.length ) {
+            return "no active plugins";
+        }
 	    var links = "<ol>";
-	    for (var i=0; i < plugins.length; i++) {
-		    links += "<li><a href=\"javascript:";
-		    links += "Manager.switch_plugin([[true,";
-            links += "'plugin switched to: "+plugins[i];
-            links += "'],'"+plugins[i]+"']);\">";
-		    links += plugins[i]+"</a></li>";
-	    }
+        for (var i=0; i < items.length; i++) {
+            links += "<li><a href=\"javascript:";
+            links += prescript + items[i];
+            links += midscript + items[i];
+            if ( postscript ) {
+                links += postscript + items[i];
+            }
+            links += "</a></li>";
+        }
 	    links += "</ol>";
 	    return links;
     }
