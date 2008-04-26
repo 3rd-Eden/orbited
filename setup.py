afe8b700e52b2db8e55828a21c829aa7ad0ad514
heyadayo <heyadayo@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-04-26 03:39:38 +0000

    package tweaks

---
+++ setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+import os
+
+static_types = [
+    '*.js', 
+    '*.html',
+    '*.css', 
+    '*.ico', 
+    '*.gif', 
+    '*.jpg', 
+    '*.png', 
+    '*.txt*',
+]
+
+setup(
+    name='orbited',
+    version='0.4.1.twisted',
+    author='Michael Carter',
+    author_email='CarterMichael@gmail.com',
+    url='http://www.orbited.org',
+    download_url='http://www.orbited.org/download',
+    license='MIT License',
+    description='A twisted-based comet server',
+    long_description='',
+    packages= find_packages(),
+    package_data = {'': reduce(list.__add__, [ '.svn' not in d and [ os.path.join(d[len('orbited')+1:], e) for e in
+            static_types ] or [] for (d, s, f) in os.walk(os.path.join('orbited', 'static'))
+        ]) },
+    zip_safe = False,
+    install_requires = [
+        # "event >= 0.3"
+    ],
+    
+    entry_points = '''    
+        [console_scripts]
+        orbited = orbited.start:main
+    ''',
+    
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],        
+)
