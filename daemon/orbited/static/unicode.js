0ddcd27cd9d868e59f071956a2048b89f2a99afc
marcus <marcus@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-06-10 23:21:07 +0000

    Replacement Unicode Implementation

--- daemon/orbited/static/unicode.js
+++ daemon/orbited/static/unicode.js
@@ -1,3 +1,19 @@
+// Copyright (C) 2008 Marcus Cavanaugh and Orbited
+// 
+// This library is free software; you can redistribute it and/or modify it under the
+// terms of the GNU Lesser General Public License as published by the Free Software
+// Foundation; either version 2.1 of the License, or (at your option) any later
+// version.
+// 
+// This library is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with
+// this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+// Suite 330, Boston, MA 02111-1307 USA
+
+
 fromBin = function(bin) { val = 0; for (var i = bin.length-1; i >= 0; i--) { if (bin[bin.length-i-1] == '1') { val += Math.pow(2, i) } } return val }
 
 charByteLength = function(byte) {
@@ -12,81 +28,72 @@ charByteLength = function(byte) {
     throw new Error("Invalid UTF-8 encoded data...")
 }
 
-bytesToUTF8 = function(bytes) {
-    var output = []
-    for (var i = 0; i < bytes.length; ++i) {
-        var startByte = bytes[i]
-        var val = null;
-        var len = charByteLength(startByte) 
-        switch (len) {
-            case 1:
-                val = bytes[i];
-                break;
-            case 2:
-                val = ((bytes[i] - 192) << 6)  + (bytes[i+1] - 128)
-                break
-            case 3:
-                val = ((bytes[i] - 224) << 12) + ((bytes[i+1] - 128) << 6) + (bytes[i+2] - 128)
-                break
-            case 4:
-                val = ((bytes[i] - 240) << 18) + ((bytes[i+1] - 128) << 12) + ((bytes[i+2] - 128) << 6) + (bytes[i+3] - 128)
-                break
-        }
-        i += (len -1)
-        output.push(String.fromCharCode(val))
+function bytesToUTF8(bytes) {    
+    var ret = [];
+    
+    function pad6(str) {
+        while(str.length < 6) { str = "0" + str; } return str;
     }
-    return output.join("")
+    
+    for (var i=0; i < bytes.length; i++) {
+        if ((bytes[i] & 0xf8) == 0xf0) {
+            ret.push(String.fromCharCode(parseInt(
+                         (bytes[i] & 0x07).toString(2) +
+                  pad6((bytes[i+1] & 0x3f).toString(2)) +
+                  pad6((bytes[i+2] & 0x3f).toString(2)) +
+                  pad6((bytes[i+3] & 0x3f).toString(2))
+                , 2)));
+            i += 3;
+        } else if ((bytes[i] & 0xf0) == 0xe0) {
+            ret.push(String.fromCharCode(parseInt(
+                         (bytes[i] & 0x0f).toString(2) +
+                  pad6((bytes[i+1] & 0x3f).toString(2)) +
+                  pad6((bytes[i+2] & 0x3f).toString(2))
+                , 2)));
+            i += 2;
+        } else if ((bytes[i] & 0xe0) == 0xc0) {
+                ret.push(String.fromCharCode(parseInt(
+                       (bytes[i] & 0x1f).toString(2) +
+                pad6((bytes[i+1] & 0x3f).toString(2), 6)
+                , 2)));
+            i += 1;
+        } else {
+            ret.push(String.fromCharCode(bytes[i]));
+        }
+    }
+    
+    return ret.join("");
 }
 
-
-UTF8ToBytes = function(str) {
-    var output = []
-    for (var i = 0; i < str.length; ++i) {
-        var val = str.charCodeAt(i)
-        if (val < 128) { // largest 1-byte unicode value
-            output.push(val)
-            continue
-        }
-        if (val < 2047) { // largest 2-byte unicode value
-            var firstByte = (val >> 6) + 192
-            var secondByte = val - (firstByte << 6)
-            output.push(firstByte)
-            output.push(secondByte)
-            continue
-        }
-        if (val < 65535) { // largest 3-byte unicode value
-            var firstChunk = (val >> 12)
-            var temp1 = (firstChunk << 12)
-            var secondChunk = (val - temp1) >> 6
-            var thirdChunk = (val - temp1 - (secondChunk << 6))
-            var firstByte = firstChunk + 224
-            var secondByte = secondChunk + 128
-            var thirdByte = thirdChunk + 128
-            output.push(firstByte)
-            output.push(secondByte)
-            output.push(thirdByte)
-            continue
-        }
-        if (val < 2097151 ) { // largest 4-byte unicode value
-            var firstChunk = (val >> 18)
-            var temp1 = (firstChunk << 18)
-            var secondChunk = (val - temp1) >> 12
-            var temp2 = secondChunk << 12
-            var thirdChunk = (val - temp1 - temp2) >> 6
-            var fourthChunk = (val - temp1 - temp2 - (thirdChunk << 6))
-            var firstByte = firstChunk + 240
-            var secondByte = secondChunk + 128
-            var thirdByte = thirdChunk + 128
-            var fourthByte = fourthChunk + 128
-            output.push(firstByte)
-            output.push(secondByte)
-            output.push(thirdByte)
-            output.push(fourthByte)
-            continue
-        }
-        throw new Error("Invalid UTF-8 string")
+function UTF8ToBytes(text) {
+    var ret = [];
+    
+    function pad(str, len) {
+        while(str.length < len) { str = "0" + str; } return str;
     }
-    return output
+    
+    for (var i=0; i < text.length; i++) {
+        var chr = text.charCodeAt(i);
+        if (chr <= 0x7F) {
+            ret.push(chr);
+        } else if(chr <= 0x7FF) {
+            var binary = pad(chr.toString(2), 11);
+            ret.push(parseInt("110"   + binary.substr(0,5), 2));
+            ret.push(parseInt("10"    + binary.substr(5,6), 2));
+        } else if(chr <= 0xFFFF) {
+            var binary = pad(chr.toString(2), 16);
+            ret.push(parseInt("1110"  + binary.substr(0,4), 2));
+            ret.push(parseInt("10"    + binary.substr(4,6), 2));
+            ret.push(parseInt("10"    + binary.substr(10,6), 2));
+        } else if(chr <= 0x10FFFF) {
+            var binary = pad(chr.toString(2), 21);
+            ret.push(parseInt("11110" + binary.substr(0,3), 2));
+            ret.push(parseInt("10"    + binary.substr(3,6), 2));
+            ret.push(parseInt("10"    + binary.substr(9,6), 2));
+            ret.push(parseInt("10"    + binary.substr(15,6), 2));
+        }
+    }
+    return ret;
 }
 
     orig = "당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 새로운 가정에 환영당신의 "
