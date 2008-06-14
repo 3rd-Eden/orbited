888fb08aac88c5f0d4ea4b267204ab38ee818055
mario <mario@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-06-14 05:28:34 +0000

    VEERY rudimentary xmpp...

--- daemon/orbited/static/protocols/xmpp/xmpp.js
+++ daemon/orbited/static/protocols/xmpp/xmpp.js
@@ -1,1 +1,39 @@
+ENDERS = ['>','</iq>']
+REPLIES = ["<iq type='get'><query xmlns='jabber:iq:auth'><username>mar</username></query></iq>","<iq type='set'><query xmlns='jabber:iq:auth'><username>mar</username><password>pass</password><resource>laptop</resource></query></iq>"]
+INTRO = "<stream:stream to='localhost' xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams'>"
 
+XMPPClient = function() {
+    var self = this
+    var conn = null
+    self.buffer = ""
+    self.status = 0
+
+    self.connect = function(host, port) {
+        conn = new self.transport(host, port)
+        conn.onread = read
+        conn.onopen = open
+        conn.onclose = close
+    }
+    var read = function(evt) {
+        var s = bytesToUTF8(evt)
+        self.buffer += s
+        if (s.indexOf(ENDERS[self.status]) != -1) {
+            self.status += 1
+            console.log("S: "+self.buffer)
+            self.buffer = ""
+            send(REPLIES[self.status])
+        }
+    }
+    var open = function(evt) {
+        send(INTRO)
+        console.log("C: "+INTRO)
+    }
+    var close = function(evt) {
+        console.log("connection closed")
+    }
+    var send = function(s) {
+        conn.send(UTF8ToBytes(s))
+    }
+}
+
+XMPPClient.prototype.transport = BinaryTCPConnection
