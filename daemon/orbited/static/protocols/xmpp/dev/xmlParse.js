8363898aec3da1142222022449883e5b2d3f60e8
heyadayo <heyadayo@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-06-14 05:27:49 +0000

    Working xml parser

--- daemon/orbited/static/protocols/xmpp/dev/xmlParse.js
+++ daemon/orbited/static/protocols/xmpp/dev/xmlParse.js
@@ -4,40 +4,32 @@ xhr.open('GET', '/_/static/protocols/xmpp/dev/testData.xml', true)
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
         testData = xhr.responseText
-        console.log("got testData, " + testData.length + " bytes")
         startTimer()
     }
 }
 xhr.send(null);
 var index = 0;
-var READ_INCREMENT = 100;
+var READ_INCREMENT = 10000;
 var TIMER_INCREMENT = 100;
 var timer = null;
 function startTimer() {
     timer = setInterval(doRead, TIMER_INCREMENT )
 }
 
+
+// How to use the parser Start
+var parser = new XMLStreamParser()
+parser.onread = function(node) {
+    console.log('receivedNode: ', node)
+}
+// End
+
+
 function doRead() {
     var data = testData.slice(index, index+READ_INCREMENT);
     index+= READ_INCREMENT;
-    dataReceived(data);
+    parser.receive(data);
     if (index > testData.length) {
         clearInterval(timer);
-        console.log('finished reading data');
     }
-}
-
-var buffer = ""
-function dataReceived(data) {
-    buffer += data
-    // CODE HERE
-    console.log('read! buffer.length: ' + buffer.length)
-    if (false) {
-        elementReceived(element);
-    }
-}
-
-
-function elementReceived(rootNode) {
-    console.log("Element Received:", rootNode)
 }
---
+++ daemon/orbited/static/protocols/xmpp/parser.js
@@ -0,0 +1,34 @@
+XMLStreamParser = function() {
+    var self = this;
+    var buffer = ""
+    var parser=new DOMParser();
+
+    self.onread = function(node) { }
+
+    self.receive = function(data) {
+        buffer += data
+        parseBuffer()
+    }
+
+    var parseBuffer = function() {
+        while (true) {
+            var tagOpenStartIndex = buffer.indexOf('<')
+            var tagOpenEndIndex = buffer.indexOf('>', tagOpenStartIndex)
+            if (tagOpenEndIndex == -1) {
+                return
+            }
+            var endTagNameIndex = Math.min(buffer.indexOf(' ', tagOpenStartIndex), tagOpenEndIndex)
+            var tagName = buffer.slice(tagOpenStartIndex+1, endTagNameIndex)
+            var endTag = '</' + tagName + '>'
+            var endTagIndex = buffer.indexOf(endTag)
+            if (endTagIndex== -1) {
+                return
+            }
+            var nodePayload= buffer.slice(tagOpenStartIndex, endTagIndex + endTag.length)
+            var rootNode =parser.parseFromString(nodePayload,"text/xml");
+            buffer = buffer.slice(endTagIndex + endTag.length+1)
+            self.onread(rootNode.childNodes[0]);
+        }
+    }
+
+}
