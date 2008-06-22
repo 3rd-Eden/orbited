e123d1f6978edd8541f6d96cec9e81fcd5840c3b
heyadayo <heyadayo@fe34127c-c842-0410-aa83-9fc675b9a81c>
2008-06-22 04:30:25 +0000

    added WebSocket implementation

---
+++ daemon/orbited/static/WebSocketImplementation.js
@@ -0,0 +1,33 @@
+WebSocket = function(url) {
+    var self = this;
+    self.onopen = function() { }
+    self.onclose = function() { }
+    self.onread = function(data) { console.log(data) }
+    self.readyState = 0
+
+    var conn = new BaseTCPConnection()
+
+    self.send = function(data) {
+        conn.send(data);
+    }
+    conn.onread = function(data) {
+        self.onread(data)
+    }
+    conn.onclose = function() {
+        self.readyState = conn.readyState
+        self.onclose();
+    }
+    conn.onopen = function() {
+        self.readyState = conn.readyState
+        conn.send(url)
+        self.onopen()
+    }
+    var connUrl = new URL(location.href)
+    if (typeof(ORBITED_DOMAIN) != "undefined") 
+        connUrl.domain = ORBITED_DOMAIN
+    if (typeof(ORBITED_PORT) != "undefined")
+        connUrl.port = ORBITED_PORT
+    connUrl.path = "/websocket"
+    connUrl.qs = ""
+    conn.connect(connUrl.render())
+}
