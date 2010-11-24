561b161a210d5a5ce3ec79818aaef0f7e6106fc3
desmaj <matthew.desmarais@gmail.com>
2010-11-24 17:15:43 -0500

    removed explicit dependency on coverage

--- daemon/setup.py
+++ daemon/setup.py
@@ -36,8 +36,7 @@ try:
             ]) },
         zip_safe = False,
         install_requires = ['morbid >= 0.8.7.3',
-                            'coverage == 3.3.1',
-                            'twisted==10.1'],
+                            'twisted==10.1']
         entry_points = '''
             [console_scripts]
             orbited = orbited.start:main
